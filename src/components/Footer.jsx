b7f02ad811c466f0d5ec76a33b16f51d06fe14db
Lucas Frey Torres Hanson <lucasfth@bjelke-torres.com>
2025-03-06 13:02:28 +0100

    Add webpage info

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,32 +3,42 @@ import './Footer.css';
 
 function Footer() {
   return (
-    <footer className="common-container">
-      <div className="inner-container">
-        <h2>🔗 Socials</h2>
-        {/* Social Links vertically spread out*/}
-        <div className="social-links">
-          <a href="https://instagram.com/lucas_hanson_" target="_blank" rel="noreferrer">
-          🖼️ Instagram
-          </a>
-          <a href="https://www.youtube.com/@lucashanson7590" target="_blank" rel="noreferrer">
-          📽️ YouTube
-          </a>
-          <a href="mailto:contact@lucashanson.dk" target="_blank" rel="noreferrer">
-          📧 Email
-          </a>
-          <a href="https://www.linkedin.com/in/lucas-frey-torres-hanson-b6b79320b/" target="_blank" rel="noreferrer">
-          🪧 LinkedIn
-          </a>
-          <a href="https://github.com/lucasfth" target="_blank" rel="noreferrer">
-          💻 GitHub
-          </a>
-          <a href="https://linktr.ee/lucashanson" target="_blank" rel="noreferrer">
-          🌴 Linktree
-          </a>
+    <>
+      <footer className='common-container'>
+        <div className='inner-container'>
+          <h2>🔗 Socials</h2>
+          {/* Social Links vertically spread out*/}
+          <div className='social-links'>
+            <a href='https://instagram.com/lucas_hanson_' target='_blank' rel='noreferrer'>
+            🖼️ Instagram
+            </a>
+            <a href='https://www.youtube.com/@lucashanson7590' target='_blank' rel='noreferrer'>
+            📽️ YouTube
+            </a>
+            <a href='mailto:contact@lucashanson.dk' target='_blank' rel='noreferrer'>
+            📧 Email
+            </a>
+            <a href='https://www.linkedin.com/in/lucas-frey-torres-hanson-b6b79320b/' target='_blank' rel='noreferrer'>
+            🪧 LinkedIn
+            </a>
+            <a href='https://github.com/lucasfth' target='_blank' rel='noreferrer'>
+            💻 GitHub
+            </a>
+            <a href='https://linktr.ee/lucashanson' target='_blank' rel='noreferrer'>
+            🌴 Linktree
+            </a>
+          </div>
         </div>
-      </div>
-    </footer>
+      </footer>
+      <footer className='common-container'>
+        <div className='inner-container'>
+          <p>
+            Built using React and hosted trough GitHub Pages<br/>
+            © Lucas Hanson {new Date().getFullYear()}<br/>
+            <a href='https://github.com/lucasfth/portfolio' target='_blank' rel='noreferrer'>GitHub repository</a>
+          </p></div>
+      </footer>
+    </>
   );
 }
 
